5b0f2c25aeb970a737e01f3cb358e192e3e44a33
Selim <varekm@mail.ru>
2020-12-12 12:08:48 +0300

    some changes

--- app/src/main/java/com/example/goodshofftest/service/CouchesService.java
+++ app/src/main/java/com/example/goodshofftest/service/CouchesService.java
@@ -12,6 +12,6 @@ public interface CouchesService {
       Single<GoodsInfo> getItemsSortByPriceDesc();
       @GET("get_products_new?category_id=320&sort_by=price&sort_type=asc&limit=20&offset=0&device_id=3a7612bcc84813b5&isAndroid=true&app_version=1.8.16&location=19&xhoff=36f40b3d665cdb9435904796172dde5e3f13aa8a%3A4630")
       Single<GoodsInfo> getItemsSortByPriceAsc();
-      @GET("get_products_new?category_id=320&sort_by=discount&sort_type=asc&limit=20&offset=0&device_id=3a7612bcc84813b5&isAndroid=true&app_version=1.8.16&location=19&xhoff=36f40b3d665cdb9435904796172dde5e3f13aa8a%3A4630")
+      @GET("get_products_new?category_id=320&discount=y&sort_type=desc&limit=20&offset=0&device_id=3a7612bcc84813b5&isAndroid=true&app_version=1.8.16&location=19&xhoff=36f40b3d665cdb9435904796172dde5e3f13aa8a%3A4630")
       Single<GoodsInfo> getItemsSortByDiscount();
 }
